e94451ad218fe206089f0270ef28f05e6bf54cb5
Matt Koski <mjkekai@gmail.com>
2021-08-07 10:03:59 -0700

    adds react app folders

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = (props) => {
+  return (
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
